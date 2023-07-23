6a6ae286287da135531264976a2cbfdb656f53c6
soupypasta <74085763+soupypasta@users.noreply.github.com>
2023-07-23 22:58:14 +0800

    Updated router

--- backend/app/dbrouters.py
+++ backend/app/dbrouters.py
@@ -1,11 +1,8 @@
 class server1Router:
-    """
-    A router to control all database operations on models in the
-    user application.
-    """
+
     def db_for_read(self, model, **hints):
         """
-        Attempts to read user models go to users_db.
+        Attempts to read file models go to server1.
         """
         if model._meta.app_label == 'FileServer1':
             return 'server1'
@@ -13,7 +10,7 @@ class server1Router:
 
     def db_for_write(self, model, **hints):
         """
-        Attempts to write user models go to users_db.
+        Attempts to write file models go to server1.
         """
         if model._meta.app_label == 'FileServer1':
             return 'server1'
@@ -21,7 +18,7 @@ class server1Router:
 
     def allow_relation(self, obj1, obj2, **hints):
         """
-        Allow relations if a model in the user app is involved.
+        Allow relations if a model in the app is involved.
         """
         if obj1._meta.app_label == 'FileServer1' or \
            obj2._meta.app_label == 'FileServer1':
@@ -30,7 +27,7 @@ class server1Router:
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         """
-        Make sure the auth app only appears in the 'users_db'
+        Make sure the auth app only appears in the 'server1'
         database.
         """
         if app_label == 'FileServer1':
